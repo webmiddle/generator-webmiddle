833df91e21aed82041abcce9a375b5f7825bac30
Manuel Dell'Elce <mh025r0x@gmail.com>
2017-04-10 13:40:05 +0200

    context

--- generators/app/templates/src/index.js
+++ generators/app/templates/src/index.js
@@ -1,21 +1,19 @@
 // TODO: use the framework to define a search project with Main.js as entrypoint
 
 //import { SearchProject } from 'webmiddle-project-search';
-import WebMiddle from 'webmiddle';
+import WebMiddle, { evaluate, createContext } from 'webmiddle';
 import path from 'path';
 import fs from 'fs';
 import FetchPageLinks from './FetchPageLinks';
 
 const webmiddle = new WebMiddle();
-
-webmiddle.evaluate(
+evaluate(createContext(webmiddle, { expectResource: true }), (
   <FetchPageLinks
     url={process.argv[2]}
     query={process.argv[3]}
     waitFor={process.argv[4]}
   />,
-{ expectResource: true })
-.then(outputResource => {
+)).then(outputResource => {
   console.log(
     JSON.stringify(outputResource.content, null, 2)
   );
--- generators/app/templates/tests/index.js
+++ generators/app/templates/tests/index.js
@@ -1,18 +1,16 @@
 import test from 'ava';
 import FetchPageLinks from '../src/FetchPageLinks';
-import WebMiddle from 'webmiddle';
+import WebMiddle, { evaluate, createContext } from 'webmiddle';
 
 test.beforeEach(t => {
   t.context.webmiddle = new WebMiddle();
 });
 
 test('FetchPageLinks', async t => {
-  await t.context.webmiddle.evaluate((
+  await evaluate(createContext(t.context.webmiddle, { expectResource: true }), (
     <FetchPageLinks
       url="https://news.ycombinator.com/"
       query="javascript"
     />
-  ), {
-    expectResource: true,
-  });
+  ));
 });
